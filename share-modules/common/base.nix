fff099412846957836ba04d1c2d504cb3ad86b37
Jinser Kafka <aimer@purejs.icu>
2025-11-21 03:25:30 +0800

    deploy: add user `deploy` for CI/CD

--- share-modules/common/base.nix
+++ share-modules/common/base.nix
@@ -1,13 +1,43 @@
+{ pkgs, ... }:
+
 {
   time.timeZone = "Asia/Shanghai";
 
   # from whonix
   environment.etc.machine-id.text = "b08dfa6083e7567a1921a715000001fb";
-
   programs.command-not-found.enable = false;
+
+  users.users.deploy = {
+    isNormalUser = true;
+    group = "deploy";
+    home = "/var/lib/deploy";
+    shell = pkgs.bash;
+    description = "Deployment user for CI/CD";
+  };
+  users.groups.deploy = { };
+
   security.sudo-rs = {
     enable = true;
-    execWheelOnly = true;
+    execWheelOnly = false;
     wheelNeedsPassword = true;
+    extraRules = [
+      {
+        users = [ "deploy" ];
+        commands = [
+          {
+            command = "/run/current-system/bin/switch-to-configuration";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+    extraConfig = ''
+      # Deploy user passwordless sudo for deploy-rs commands
+      deploy ALL=(ALL) NOPASSWD: /nix/var/nix/profiles/system/bin/activate-rs activate *
+      deploy ALL=(ALL) NOPASSWD: /nix/var/nix/profiles/system/bin/activate-rs wait *
+      deploy ALL=(ALL) NOPASSWD: /nix/store/*/bin/activate-rs activate *
+      deploy ALL=(ALL) NOPASSWD: /nix/store/*/bin/activate-rs wait *
+      deploy ALL=(ALL) NOPASSWD: /run/current-system/sw/bin/rm /tmp/deploy-rs*
+    '';
   };
 }
