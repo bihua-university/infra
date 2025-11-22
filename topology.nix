73f78cc660d47ce007d0c300eba33a10b5f13d33
Jinser Kafka <aimer@purejs.icu>
2025-11-22 10:23:41 +0800

    topology: remove internet nodes

--- topology.nix
+++ topology.nix
@@ -6,44 +6,39 @@
 {
   imports = [ inputs.nix-topology.flakeModule ];
 
-  perSystem = {
-    topology.modules = [
-      (
-        { config, ... }:
-        let
-          tlib = config.lib.topology;
-        in
-        {
-          networks.tailscale = {
-            name = "Tailscale Net";
-            cidrv4 = "100.64.0.0/10";
-          };
+  perSystem =
+    { pkgs, ... }:
+    {
+      topology.modules = [
+        (
+          { config, ... }:
+          let
+            tlib = config.lib.topology;
+          in
+          {
+            networks.tailscale = {
+              name = "Tailscale Net";
+              cidrv4 = "100.64.0.0/10";
+            };
 
-          nodes.internet-at-jdcloud = tlib.mkInternet {
-            connections = tlib.mkConnection "cosimo" "ens5";
-          };
-          nodes.internet-at-mie = tlib.mkInternet {
-            connections = tlib.mkConnection "sheepro" "eth0@if62";
-          };
-
-          nodes.cosimo = {
-            interfaces = {
-              tailscale0 = {
-                network = "tailscale";
-                physicalConnections = [
-                  (tlib.mkConnection "sheepro" "tailscale0")
-                ];
+            nodes.cosimo = {
+              interfaces = {
+                tailscale0 = {
+                  network = "tailscale";
+                  # renderer.hidePhysicalConnections = true;
+                  physicalConnections = [
+                    (tlib.mkConnection "sheepro" "tailscale0")
+                  ];
+                };
               };
-              ens5 = { };
             };
-          };
-          nodes.sheepro = {
-            interfaces = {
-              "eth0@if62" = { };
+            nodes.sheepro = {
+              interfaces = {
+                tailscale0.network = "tailscale";
+              };
             };
-          };
-        }
-      )
-    ];
-  };
+          }
+        )
+      ];
+    };
 }
