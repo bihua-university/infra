a8d98e1ad2fe024822de5141391f313dfe527397
Jinser Kafka <aimer@purejs.icu>
2025-11-22 15:35:03 +0800

    refactor: cosimo.sops

--- hosts.d/cosimo/sops.nix
+++ hosts.d/cosimo/sops.nix
@@ -4,24 +4,7 @@
 }:
 
 {
-  imports = [
-    flake.inputs.sops-nix.nixosModules.sops
-  ];
+  imports = [ flake.inputs.sops-nix.nixosModules.sops ];
 
-  sops = {
-    defaultSopsFile = ./secrets.yaml;
-    secrets = {
-      # jinserMailPWD = { };
-      # noreplyMailPWD = { };
-
-      # sendgridApiKey = {
-      #   mode = "0440";
-      #   group = groups.mailer.name;
-      # };
-      # qcloudmailPWD = {
-      #   mode = "0440";
-      #   group = groups.mailer.name;
-      # };
-    };
-  };
+  sops.defaultSopsFile = ./secrets.yaml;
 }
