6bf4cad89f5fe3f87a0515cc98a6d012f6d5c53d
Jinser Kafka <aimer@purejs.icu>
2025-11-22 08:23:08 +0800

    deploy: act user deploy as root

--- share-modules/common/base.nix
+++ share-modules/common/base.nix
@@ -17,36 +17,25 @@
     home = "/var/lib/deploy";
     shell = pkgs.bash;
     description = "Deployment user for CI/CD";
+    extraGroups = [ "wheel" ];
   };
   users.groups.deploy = { };
 
-  # NOTE: from sudo-rs SUDOERS(5)
-  # Wildcards in command line arguments are not supported—using these in original versions of sudo was usually a sign
-  # of mis-configuration and consequently sudo-rs simply forbids using them.
-  # no need to do this acctually...the hosts behind tailscale ACL
-  security.sudo = {
+  security.sudo-rs = {
     enable = true;
     execWheelOnly = true;
     wheelNeedsPassword = true;
-    # Deploy user passwordless sudo for deploy-rs commands
-    extraRules =
-      let
-        mkDeployRule = command: {
-          users = [ "deploy" ];
-          runAs = "root";
-          commands = [
-            {
-              inherit command;
-              options = [ "NOPASSWD" ];
-            }
-          ];
-        };
-      in
-      [
-        (mkDeployRule "/run/current-system/bin/switch-to-configuration")
-        (mkDeployRule "/nix/store/*/activate-rs activate *")
-        (mkDeployRule "/nix/store/*/activate-rs wait *")
-        (mkDeployRule "/run/current-system/sw/bin/rm /tmp/deploy-rs*")
-      ];
+    extraRules = [
+      {
+        users = [ "deploy" ];
+        runAs = "root";
+        commands = [
+          {
+            command = "ALL";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
   };
 }
