82e4947fd8d3f138561092596b303af0e519835e
Jinser Kafka <aimer@purejs.icu>
2026-03-12 00:28:41 +0800

    caddy: update hash

--- hosts.d/cosimo/services/default.nix
+++ hosts.d/cosimo/services/default.nix
@@ -23,7 +23,7 @@ in
     enable = cfg.caddy.virtualHosts != { };
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
+      hash = "sha256-biQdtiscdmrwP6VUsuXmZrvcdewl+g50kdmab5lhE0s=";
     };
     environmentFile = secrets.caddy.path;
     virtualHosts."(tsnet)".extraConfig = ''
