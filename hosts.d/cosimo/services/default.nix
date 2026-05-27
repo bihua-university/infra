20e1a907cdc6fb6df691daa387e6c3ec1f8f849f
Jinser Kafka <aimer@purejs.icu>
2026-05-27 16:22:30 +0800

    services(cosimo): cleanup

--- hosts.d/cosimo/services/default.nix
+++ hosts.d/cosimo/services/default.nix
@@ -1,47 +1,4 @@
 {
-  config,
-  lib,
-  pkgs,
-  flake,
-  ...
-}:
-
-let
-  cfg = config.services;
-
-  inherit (config.sops) secrets;
-in
-{
-  sops.secrets = lib.mkIf cfg.caddy.enable {
-    caddy = {
-      sopsFile = flake.commonSecrets + /caddy.env;
-      format = "dotenv";
-    };
-  };
-
-  services.caddy = {
-    enable = cfg.caddy.virtualHosts != { };
-    package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-biQdtiscdmrwP6VUsuXmZrvcdewl+g50kdmab5lhE0s=";
-    };
-    environmentFile = secrets.caddy.path;
-    virtualHosts."(tsnet)".extraConfig = ''
-      @blocked not remote_ip 100.64.0.0/10
-
-      tls {
-        dns cloudflare {$CLOUDFLARE_API_TOKEN}
-      }
-
-      respond @blocked "Unauthorized" 403
-    '';
-  };
-
-  networking.firewall.allowedTCPPorts = lib.mkIf cfg.caddy.enable [
-    80
-    443
-  ];
-
   preservation.preserveAt."/persist" = {
     directories = [ "/var/lib" ];
   };
