b5da1798942d0a1578c08f1cb98727ba11cd4980
Jinser Kafka <aimer@purejs.icu>
2026-06-01 01:13:26 +0800

    qque(sheepro): disable

--- hosts.d/sheepro/services/qque.nix
+++ hosts.d/sheepro/services/qque.nix
@@ -20,24 +20,16 @@ in
 
   nixpkgs.superConfig.allowUnfreeList = [ "qq" ];
 
-  sops.secrets.qqNumber = {
-    owner = cfg.user;
-    group = cfg.group;
-    mode = "0400";
+  sops.secrets = lib.mkIf cfg.enable {
+    qqNumber = {
+      owner = cfg.user;
+      group = cfg.group;
+      mode = "0400";
+    };
   };
   services.quasique = {
-    enable = true;
+    enable = false;
     qqPath = secrets.qqNumber.path;
-  };
-
-  services.caddy.virtualHosts = lib.mkIf cfg.enable {
-    "qq.bhu.social".extraConfig = ''
-      import tsnet
-      reverse_proxy http://sheepro:${toString cfg.port} {
-        header_down X-Real-IP {http.request.remote}
-        header_down X-Forwarded-For {http.request.remote}
-      }
-    '';
   };
 
   topology.self = lib.mkIf cfg.enable {
